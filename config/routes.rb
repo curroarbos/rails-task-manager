f1849d69dcebc7fbaa3c7557ea495a0abe6308f8
Francisco Arbos Ortega <curroarbos@gmail.com>
2023-11-14 14:11:46 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # get 'task/:id', to: 'tasks#show', as: 'task'
+  # post 'tasks', to: 'tasks#create'
+  # get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'task/:id', to: 'tasks#update'
+  # delete 'task/:id', to: 'tasks#destroy', as: 'delete_task'
+
+  resources :tasks
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
